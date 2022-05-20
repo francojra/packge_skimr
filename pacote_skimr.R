5c59e317c96030855bae06088f85255c1dadcec8
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-19 21:39:54 -0300

    Add files via upload

--- pacote_skimr.R
+++ pacote_skimr.R
@@ -15,3 +15,21 @@ library(skimr)
 ### estamos usando a base dados_starwars do pacote dados.
 
 dados::dados_starwars |> skim()
+
+### Veja que a saída da função skim mostra uma visão geral da base de dados, 
+### nos dando informações como número de linhas, número de colunas, e os tipos das colunas.
+
+### Ela fornece também informações individuais sobre cada coluna da base, separando as
+### colunas por tipo: cada tipo nos dá um conjunto diferente de estatísticas, que façam
+### sentido para aquele tipo de dado.
+
+### Além disso, se salvarmos a saída da função em um objeto, podemos obter um data frame 
+### no qual cada linha é referente a uma coluna da base de dados.
+
+### Isso é muito útil quando estamos lidando com bases que tem muitas colunas.
+
+# Visualizando as informações dos dados em data.frame --------------------------------------------------------------------------------------
+
+info_skim <- dados::dados_starwars |> 
+  skim() |> 
+  tibble::as_tibble()
